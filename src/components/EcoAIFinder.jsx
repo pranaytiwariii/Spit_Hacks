f7eebec688e144ffc1ba0f99d3b7690815c30879
Manjesh501 <manjesht78@gmail.com>
2024-11-10 08:03:34 +0530

    final

--- src/components/EcoAIFinder.jsx
+++ src/components/EcoAIFinder.jsx
@@ -68,7 +68,7 @@ export default function EcoAIFinder() {
 
   const generateRecommendations = async () => {
     try {
-      const response = await fetch('/api/recommendations', {
+      const response = await fetch('http://127.0.0.1:8000/recommendations', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
